44a60d735a5e419bede4495f02ec3beb6e1513dc
imbassado <edualansatos@gmail.com>
2026-06-18 16:34:01 -0300

    config: adiciona o endereço do front ao CORS

--- back-end/src/main/java/com/protoserv/config/SecurityConfig.java
+++ back-end/src/main/java/com/protoserv/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://protoserv.up.railway.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
